c02bc50a7194c0b8ebd017450be6776213a2fdc7
Sergio Tapia-Fikes <stapiafikes@gmail.com>
2021-06-20 21:23:33 -0500

    Python Elements Of Programming: forLoop.py updates with notes on range.

--- LanguageBasics/forLoop.py
+++ LanguageBasics/forLoop.py
@@ -1,7 +1,14 @@
 # range(start, stop, step)
 # start inclusive, stop exclusive
 # step An integer number specifying the incrementation. Default is 1
+# start is optional, stop is required and step is optional
 
-x = range(2, 6, 2)
-for n in x:
+range1 = range(2, 6, 2)
+for n in range1:
   print(n)
+
+# one argument only will be the stop 'exclusive'.
+range2 = range(5)
+
+for n in range2:
+    print(n) # print numbers 0 - 4
